aaa99f194f569312460c920471ecd4c381d32d69
Surajit <piklu3043@gmail.com>
2024-02-24 01:53:04 +0530

    Before structure

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const Product = require("./models/product.model")
 require("dotenv").config();
 const app = express()
 
+// * middlewares
 app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
 
 // ! GET ALL PRODUCTS
 app.get("/api/products", async (req, res) => {
     try {
         const products = await Product.find({})
-        res.status(200).json({ data: products ``});
+        res.status(200).json({ data: products });
     } catch (error) {
         res.status(500).json({message: error.message})
     }
@@ -22,6 +25,38 @@ app.get("/api/products/:id", async (req, res) => {
         const { id } = req.params
         const product = await Product.findById(id)
         res.status(200).json({ data: product });
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// ! UPDATE A PRODUCT
+app.put("/api/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+
+        if(!product){
+            res.status(404).json({message: `Product doesn't exist`})
+        }
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json({ data: updatedProduct });
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// ! DELETE A PRODUCT BY ID
+app.delete("/api/products/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const deletedProduct = await Product.findByIdAndDelete(id)
+
+        if(!deletedProduct){
+            res.status(404).json({message: "Product don't exist."})
+        }
+        res.status(200).json({message: "Product deleted successfully."})
+
     } catch (error) {
         res.status(500).json({message: error.message})
     }
